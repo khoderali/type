bbe8a96c1a4ea126cbedf25d72a1f418b3a6eef9
khoderali <khoderrali@gmail.com>
2024-09-28 23:58:45 -0700

    update

---
+++ script/1.js
@@ -0,0 +1,6 @@
+class App {
+    constructor() {
+    alert("If you are unable to deal with our page, you can click in the summary on the sentence you want to reach the desired section through.");
+    }
+}
+onload = new App();
